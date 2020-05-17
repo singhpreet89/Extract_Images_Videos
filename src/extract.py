2a597f9db90bc53bb5d16d2025b14fa30a775064
Harpreet Singh <singh.harpreet89@hotmail.com>
2020-05-17 03:43:05 -0400

    Optimized the code and add support for .png files

--- src/extract.py
+++ src/extract.py
@@ -3,6 +3,8 @@ from glob import glob
 from PIL import Image
 import shutil
 import sys
+from pathlib import Path
+from functools import reduce
 from dotenv import load_dotenv
 
 class Extract:
@@ -10,60 +12,43 @@ class Extract:
         load_dotenv()
 
         self.SOURCE_DIRECTORY = os.getenv("SOURCE_DIRECTORY")
-        self.TEMP = os.getenv("TEMP_DIRECTORY")
         self.DESTINATION_DIRECTORY = os.getenv("DESTINATION_DIRECTORY")
-        
-        # Minimum size of an Image to be extracted
-        self.MINIMUM_SIZE = int(os.getenv("MINIMUM_SIZE"))
+        self.MINIMUM_SIZE_IMAGE = int(os.getenv("MINIMUM_SIZE_IMAGE"))
+        self.SUPPORTED_FORMATS = ['*.' + value for value in os.getenv("SUPPORTED_FORMATS").split(',')]
+        self.EXCEPTIONS = dict()
 
-        self.SAVE_FORMATS = ['*.' + value for value in os.getenv("SAVE_FORMATS").split(',')]
-        self.CONVERT_TO_JPG = ['*.' + value for value in os.getenv("CONVERT_TO_JPG").split(',')]
-    
-        self.EXCEPTIONS = dict()                
-    
-    def copy_to_temp(self):
+        try:
+            if not os.path.exists(os.path.normpath(os.path.join(os.getcwd(), self.SOURCE_DIRECTORY))):
+                os.makedirs(os.path.normpath(os.path.join(os.getcwd(), self.SOURCE_DIRECTORY)))
+            if not os.path.exists(os.path.normpath(os.path.join(os.getcwd(), self.DESTINATION_DIRECTORY, 'images'))):
+                os.makedirs(os.path.normpath(os.path.join(os.getcwd(), self.DESTINATION_DIRECTORY, 'images')))
+            if not os.path.exists(os.path.normpath(os.path.join(os.getcwd(), self.DESTINATION_DIRECTORY, 'videos'))):
+                os.makedirs(os.path.normpath(os.path.join(os.getcwd(), self.DESTINATION_DIRECTORY, 'videos')))
+        except:
+            print(f'\n*****\nALERT: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****')
+            sys.exit()
+
+    def save(self):
         print('Please wait.....')
 
-        # pylint: disable=unused-variable   #(Since "directories" variable is not being used)
-        for root, directories, files in os.walk(self.SOURCE_DIRECTORY):
-            for file in files:
-                try: 
-                    path_file = os.path.join(root, file)
-                    shutil.copy2(path_file, self.TEMP)
-                except:
-                    # print(f'\n*****\nALERT for FILE: {file}\nException: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****\n')
-                    self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
-        os.chdir(os.path.normpath(os.getcwd() + os.sep + self.TEMP))
-
-    def convert(self):
-        for format in self.CONVERT_TO_JPG:
-            try:
-                for file in glob(format):
-                    webp_image_name = os.path.splitext(file)[0]
-                    Image.open(file).convert("RGB").save(webp_image_name + ".jpg", "jpeg")
-            except:
-                self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
-        print("\nConversion to .jpg complete.....")    
-            
-    def extract(self):
-        for format in self.SAVE_FORMATS:
-            for file in glob(format):
+        for format in self.SUPPORTED_FORMATS:
+            for file in glob(self.SOURCE_DIRECTORY + os.sep + '**' + os.sep + format, recursive=True): 
                 try:
-                    if (format == '*.jpg' or format == '*.jpeg') and (os.path.getsize(file) / 1000) >= self.MINIMUM_SIZE:
-                        shutil.move(f'{file}', os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + str(self.DESTINATION_DIRECTORY) + os.sep + 'images'))
-                    elif format == '*.mp4':
-                        shutil.move(f'{file}', os.path.normpath(os.getcwd() + os.sep + '..' + os.sep + str(self.DESTINATION_DIRECTORY) + os.sep + 'videos'))
-                except:
-                    self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
+                    directory = os.path.normpath(os.path.join(os.getcwd(), self.DESTINATION_DIRECTORY))
+                    file_name = Path(file).stem.split('.')[0] + reduce(lambda extension, each_extension : extension + each_extension, Path(file).suffixes)  # A file could have multiple extensions (i.e. example.taz.gz)  
 
-    def delete_temp_directory(self):
-        for file in glob('*'):
-            try: 
-                os.remove(file)
-            except:
-                # print(f'\n*****\nALERT for FILE: {file}\nException: {sys.exc_info()[1]}\nException type: {sys.exc_info()[0]}\n*****\n')
-                self.EXCEPTIONS[f'File: {str(file)}'] = f'Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
-        print("Temporary files deleted.....")
+                    if (format == '*.jpg' or format == '*.jpeg') and (os.path.getsize(file) / 1000) >= self.MINIMUM_SIZE_IMAGE:
+                        shutil.copy2(file, os.path.normpath(os.path.join(directory, 'images', file_name)))
+                    elif format == '*.jfif' or format == '*.webp' or format == '*.png' and (os.path.getsize(file) / 1000) >= self.MINIMUM_SIZE_IMAGE:       # Converting .jfif, .webp and .png to .jpg format and then save
+                        if len(Path(file).suffixes) == 1:
+                            file_name = Path(file).stem.split('.')[0]
+                        elif len(Path(file).suffixes) > 1:
+                            file_name = reduce(lambda extension, each_extension : extension + '.' + each_extension, Path(file).stem.split('.'))
+                        Image.open(file).convert("RGB").save(os.path.normpath(os.path.join(directory, 'images', file_name + ".jpg")), "jpeg")
+                    elif format == '*.mp4':
+                        shutil.copy2(file, os.path.normpath(os.path.join(directory, 'videos', file_name)))
+                except:
+                    self.EXCEPTIONS[f'File: {str(file)}'] = f'EXTRACTION Exception type: {sys.exc_info()[0]}, {sys.exc_info()[1]}'
               
     def check_exceptions(self):   
         if self.EXCEPTIONS:
@@ -75,8 +60,5 @@ class Extract:
             print("\nFINISHED.....\n")
             
     def boot(self):
-        self.copy_to_temp()
-        self.convert()
-        self.extract()
-        self.delete_temp_directory()
+        self.save()
         self.check_exceptions()
